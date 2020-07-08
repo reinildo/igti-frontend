74fe0a01054bc856f885a309f52ff7bcebe24c1e
Reinildo Reis <reinildo@gmail.com>
2020-07-07 23:50:56 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+  // <BrowserRouter>
+  //   <App />
+  // </BrowserRouter>,
+  <h1>Teste</h1>,
+  document.getElementById("root")
 );
 
 serviceWorker.unregister();
